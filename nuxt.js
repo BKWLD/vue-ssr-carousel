8f3aef6065c94551ebef3b67f1601b0d7bc0853d
Robert Reinhard <robert.reinhard@bukwild.com>
2022-02-28 09:50:39 -0800

    Add a Nuxt module for easier importing
    Fixes #50, #51

---
+++ nuxt.js
@@ -0,0 +1,13 @@
+import { join } from 'path'
+export default function() {
+
+	// Allow components to be auto-imported by Nuxt
+	this.nuxt.hook('components:dirs', dirs => {
+		dirs.push({
+			path: join(__dirname, 'src')
+		})
+	})
+
+	// Add the extracted CSS globally
+	this.options.css.push('vue-ssr-carousel/index.css')
+}
