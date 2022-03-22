f49701d5417d272356d82e326d067e8957f5d538
Robert Reinhard <robert.reinhard@bukwild.com>
2022-03-22 15:02:52 -0700

    Stubbing out accessibility demo

---
+++ demo/components/demos/misc/accessibility.vue
@@ -0,0 +1,9 @@
+<template>
+
+<ssr-carousel :slides-per-page='2' show-dots show-arrows loop>
+  <slide :index='1' to='#'></slide>
+  <slide :index='2' to='#'></slide>
+  <slide :index='3' to='#'></slide>
+</ssr-carousel>
+
+</template>
