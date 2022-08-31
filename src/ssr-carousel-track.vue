05fa9a00d3625d921901be0bcb7f2e37efa71821
Robert Reinhard <robert.reinhard@bukwild.com>
2022-08-30 17:03:51 -0700

    Prevent duplicate keys on clones

--- src/ssr-carousel-track.vue
+++ src/ssr-carousel-track.vue
@@ -82,6 +82,10 @@ export default
 			if isPeekingClone or index not in @activeSlides
 			then vnode.data.attrs['aria-hidden'] = 'true'
 
+			# Prevent duplicate keys on clones
+			if isPeekingClone and vnode.key
+			then vnode.key += '-clone-' + index
+
 			# Return modified vnode
 			return vnode
 
