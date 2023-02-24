514aea75671d143b160e12a88e3320cd074086b6
Robert Reinhard <robert.reinhard@bukwild.com>
2023-02-23 16:34:49 -0800

    Document pagination-label

---
+++ demo/components/demos/accessibility/pagination-label.vue
@@ -0,0 +1,9 @@
+<template>
+
+<ssr-carousel pagination-label='Story' show-arrows show-dots loop>
+  <slide>Story 1</slide>
+  <slide>Story 2</slide>
+  <slide>Story 3</slide>
+</ssr-carousel>
+
+</template>
