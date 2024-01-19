c06374e497576cbe38d68dabde6e6ff404505d84
Robert Reinhard <robert.reinhard@bukwild.com>
2024-01-19 15:35:25 -0800

    Add RTL demo

---
+++ demo/components/demos/accessibility/rtl.vue
@@ -0,0 +1,11 @@
+<template>
+
+<div :style='{ direction: "rtl" }'>
+	<ssr-carousel rtl>
+		<slide :index='1'></slide>
+		<slide :index='2'></slide>
+		<slide :index='3'></slide>
+	</ssr-carousel>
+</div>
+
+</template>
