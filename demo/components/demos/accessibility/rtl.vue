a76c0f5ed1718286178c924cc883b4bccc206d59
Robert Reinhard <robert.reinhard@bukwild.com>
2024-01-19 16:57:08 -0800

    Show UI in the demo

--- demo/components/demos/accessibility/rtl.vue
+++ demo/components/demos/accessibility/rtl.vue
@@ -1,7 +1,7 @@
 <template>
 
 <div :style='{ direction: "rtl" }'>
-	<ssr-carousel rtl>
+	<ssr-carousel rtl show-dots show-arrows>
 		<slide :index='1'></slide>
 		<slide :index='2'></slide>
 		<slide :index='3'></slide>
